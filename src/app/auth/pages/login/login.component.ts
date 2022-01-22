09156e49ff85f4f33389ec79993d5861ba0b0a0f
uberalle <uberek@disroot.org>
2022-01-22 23:25:10 +0100

    Cambios en formulario, estructurando. Falta funcionalidad

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import {  } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent{
+  loginForm: FormGroup = new FormGroup({});
+  errorMessage = '';
+  submitted = false;
+  constructor( private router: Router, private authService: AuthService, private formBuilder: FormBuilder) { }
+  
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
-  constructor( private router: Router,
-               private authService: AuthService ) { }
+  onSubmit() {
+
+  }
 
   login() {
 
